55fb359a4ab8eeac992d4134611241f69a187d08
Sam Wolfs <be.samwolfs@gmail.com>
2019-10-06 15:40:44 +0200

    Add Exercise-1-2 TODOs

--- App.tsx
+++ App.tsx
@@ -2,6 +2,12 @@ import React from 'react';
 import { StyleSheet, View } from 'react-native';
 import { CircledImage } from './src/ui';
 
+// TODO: under the src/ui/article directory, create a new file `ArticlePreview.tsx`
+// TODO: create a new component `ArticlePreview` that accepts an article through props (check assets/articles.js for the type signature)
+// TODO: create an interface or type that describes an article. (Tip: Build a separate interface/type for the author)
+// TODO: Combine Flexbox, CircledImage and Text elements to build a reusable ArticlePreview component
+// TODO: Focus on building the layout; Favourite-button, tags and text will be styled later
+
 export default function App() {
   return (
     <View style={styles.container}>
