4fc567ef919b4b2ea65f7aeea7dfded036ed0171
Sam Wolfs <be.samwolfs@gmail.com>
2019-10-06 19:43:33 +0200

    Add Exercise-1-5 TODOs

--- App.tsx
+++ App.tsx
@@ -3,6 +3,12 @@ import { StyleSheet, View, FlatList } from 'react-native';
 import { ArticlePreview } from './src/ui/article/ArticlePreview';
 import { ARTICLES } from './assets/articles.js';
 
+// TODO: Under src/pages add ArticlesList.tsx and refactor your code by moving all ArticlesList logic to your new component
+// TODO: use react-navigation's createStackNavigator function to build our navigation stack (1 route: Home)
+// TODO: use react-navigation's createAppContainer function to create our new app container using the previously created Stack
+// TODO: Render AppContainer inside App's container View
+// TODO: In ArticlesList.tsx, set your Navigation Bar's title to 'Conduit' using NavigationScreenOptions 
+
 export default function App() {
   const articles: Article[] = ARTICLES;
   const renderItem = ({ item }: { item: Article }): JSX.Element => {
