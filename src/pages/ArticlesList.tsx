880a95e980c1cecce7291f07f0e2995668ab2ef8
Sam Wolfs <be.samwolfs@gmail.com>
2019-10-12 16:01:31 +0200

    Added navigation hook

--- src/pages/ArticlesList.tsx
+++ src/pages/ArticlesList.tsx
@@ -3,6 +3,7 @@ import { View, FlatList } from 'react-native';
 import { ArticlePreview } from '../ui';
 import { ARTICLES } from '../../assets/articles.js';
 import { styles } from './ArticlesList.styles';
+import { NavigationStackOptions } from 'react-navigation-stack';
 
 type Article = {
   title: string;
@@ -24,12 +25,12 @@ type Author = {
   following: boolean;
 };
 
-// TODO: Use the useNavigation hook to add navigation to the ArticlesList component
+// TODO: Use the useNavigation hook (src/hooks) to add navigation to the ArticlesList component
 // TODO: Write a function `navigateArticle` that navigates to the Article route, passing slug as a param {slug: slug}
 // TODO: Under src/pages create a new component ArticleDetail.tsx; for now let it display the slug you passed to it through the navigator
 // TODO: You can get route params through the navigation object of your component
 
-export const ArticlesList: React.FunctionComponent & { navigationOptions?: any } = (): JSX.Element => {
+export const ArticlesList: React.FunctionComponent & { navigationOptions?: NavigationStackOptions } = (): JSX.Element => {
   const articles: Article[] = ARTICLES;
   const renderItem = ({ item }: { item: Article }): JSX.Element => {
     return (
