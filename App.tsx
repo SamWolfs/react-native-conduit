7f681de05825cd26b79a53429f9787f010dd8fb7
Sam Wolfs <be.samwolfs@gmail.com>
2019-10-13 17:18:03 +0200

    Added user login for Exercise-2-4

--- App.tsx
+++ App.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { createAppContainer } from "react-navigation";
 import { createStackNavigator } from "react-navigation-stack";
 import ArticlesListPage from './src/pages/ArticlesList';
@@ -7,6 +7,7 @@ import { createStore, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
 import reducer from './src/reducks';
 import thunk from 'redux-thunk';
+import { loginUser } from './src/reducks/user';
 
 export default function App() {
   const Stack = createStackNavigator({
@@ -21,6 +22,11 @@ export default function App() {
   const store = createStore(reducer, applyMiddleware(thunk));
   
   const AppContainer = createAppContainer(Stack);
+
+  useEffect(() => {
+    //@ts-ignore
+    store.dispatch(loginUser({user: { username: 'swo', email: 'swo@asist.be', password: 'swoswoswo' }}));
+  }, []);
   
   return (
     <Provider store={store}>
--- src/reducks/index.ts
+++ src/reducks/index.ts
@@ -1,5 +1,6 @@
 import article from "./article";
+import user from "./user";
 import { combineReducers } from 'redux';
 
 
-export default combineReducers({article: article});
+export default combineReducers({user: user, article: article});
