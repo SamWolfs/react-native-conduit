b22c7d9ac949372d59fc789c62df71ca05eabc97
Sam Wolfs <be.samwolfs@gmail.com>
2020-09-21 09:27:12 +0200

    Comment exercise code

--- src/ui/CircledImage.tsx
+++ src/ui/CircledImage.tsx
@@ -11,9 +11,12 @@ export const CircledImage = (image: CircledImage): JSX.Element => {
   const imageStyle = {
     width: image.size,
     height: image.size,
+    // css circle: square with border-radius equal to half its width
     borderRadius: image.size / 2
   }
 
+  // The || (OR) operator can also be used to short-circuit statements
+  // it stops at the first truthy value
   return (
     <Image
           style={[imageStyle, image.style]}
