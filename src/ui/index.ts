803186d8cabf45a44b52c58e11859d3ec4d07115
Sam Wolfs <be.samwolfs@gmail.com>
2020-09-21 11:12:28 +0200

    Comment exercise code

--- src/ui/index.ts
+++ src/ui/index.ts
@@ -1,1 +1,5 @@
+
+// exporting from an index.ts file allows you to barrel your exports
+// using this technique allows you to import from a directory rather than from files
+// this creates overall cleaner imports and easier refactors as well as better structured code
 export * from './CircledImage';
